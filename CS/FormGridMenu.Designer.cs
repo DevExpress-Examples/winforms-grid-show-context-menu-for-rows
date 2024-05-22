557e003fab593113128bde26e17b4cde401224bc
AndreySlabov <andrey.slabov@devexpress.com>
2024-05-22 12:51:03 +0400

    New Context Menu API

--- CS/FormGridMenu.Designer.cs
+++ CS/FormGridMenu.Designer.cs
@@ -28,24 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormGridMenu));
             this.gridControl1 = new DevExpress.XtraGrid.GridControl();
             this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
-            this.ContextMenu1 = new System.Windows.Forms.ContextMenu();
-            this.MenuItem1 = new System.Windows.Forms.MenuItem();
-            this.MenuItem2 = new System.Windows.Forms.MenuItem();
-            this.radioGroup1 = new DevExpress.XtraEditors.RadioGroup();
+            this.popupMenu_Row = new DevExpress.XtraBars.PopupMenu(this.components);
+            this.barButtonItem_Edit = new DevExpress.XtraBars.BarButtonItem();
+            this.barButtonItem_Delete = new DevExpress.XtraBars.BarButtonItem();
+            this.barManager1 = new DevExpress.XtraBars.BarManager(this.components);
+            this.barDockControlTop = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlBottom = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlLeft = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlRight = new DevExpress.XtraBars.BarDockControl();
             ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.radioGroup1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.popupMenu_Row)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barManager1)).BeginInit();
             this.SuspendLayout();
             // 
             // gridControl1
             // 
-            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.gridControl1.Location = new System.Drawing.Point(0, 0);
             this.gridControl1.MainView = this.gridView1;
             this.gridControl1.Name = "gridControl1";
-            this.gridControl1.Size = new System.Drawing.Size(448, 317);
+            this.gridControl1.Size = new System.Drawing.Size(1015, 542);
             this.gridControl1.TabIndex = 0;
             this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.gridView1});
@@ -56,57 +63,105 @@
             this.gridView1.Name = "gridView1";
             this.gridView1.PopupMenuShowing += new DevExpress.XtraGrid.Views.Grid.PopupMenuShowingEventHandler(this.gridView1_PopupMenuShowing);
             // 
-            // ContextMenu1
+            // popupMenu_Row
             // 
-            this.ContextMenu1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
-            this.MenuItem1,
-            this.MenuItem2});
+            this.popupMenu_Row.LinksPersistInfo.AddRange(new DevExpress.XtraBars.LinkPersistInfo[] {
+            new DevExpress.XtraBars.LinkPersistInfo(this.barButtonItem_Edit),
+            new DevExpress.XtraBars.LinkPersistInfo(this.barButtonItem_Delete)});
+            this.popupMenu_Row.Manager = this.barManager1;
+            this.popupMenu_Row.Name = "popupMenu_Row";
             // 
-            // MenuItem1
+            // barButtonItem_Edit
             // 
-            this.MenuItem1.Index = 0;
-            this.MenuItem1.Text = "Edit";
-            this.MenuItem1.Click += new System.EventHandler(this.ItemEdit_Click);
+            this.barButtonItem_Edit.Caption = "Edit";
+            this.barButtonItem_Edit.Id = 0;
+            this.barButtonItem_Edit.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("barButtonItem_Edit.ImageOptions.SvgImage")));
+            this.barButtonItem_Edit.Name = "barButtonItem_Edit";
+            this.barButtonItem_Edit.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.barButtonItem_Edit_ItemClick);
             // 
-            // MenuItem2
+            // barButtonItem_Delete
             // 
-            this.MenuItem2.Index = 1;
-            this.MenuItem2.Text = "Delete";
-            this.MenuItem2.Click += new System.EventHandler(this.ItemDelete_Click);
+            this.barButtonItem_Delete.Caption = "Delete";
+            this.barButtonItem_Delete.Id = 1;
+            this.barButtonItem_Delete.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("barButtonItem_Delete.ImageOptions.SvgImage")));
+            this.barButtonItem_Delete.Name = "barButtonItem_Delete";
+            this.barButtonItem_Delete.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.barButtonItem_Delete_ItemClick);
             // 
-            // radioGroup1
+            // barManager1
             // 
-            this.radioGroup1.EditValue = "DevExpress Menu";
-            this.radioGroup1.Location = new System.Drawing.Point(454, 12);
-            this.radioGroup1.Name = "radioGroup1";
-            this.radioGroup1.Properties.Items.AddRange(new DevExpress.XtraEditors.Controls.RadioGroupItem[] {
-            new DevExpress.XtraEditors.Controls.RadioGroupItem("Standard Menu", "Standard Menu"),
-            new DevExpress.XtraEditors.Controls.RadioGroupItem("DevExpress Menu", "DevExpress Menu")});
-            this.radioGroup1.Size = new System.Drawing.Size(116, 80);
-            this.radioGroup1.TabIndex = 1;
+            this.barManager1.DockControls.Add(this.barDockControlTop);
+            this.barManager1.DockControls.Add(this.barDockControlBottom);
+            this.barManager1.DockControls.Add(this.barDockControlLeft);
+            this.barManager1.DockControls.Add(this.barDockControlRight);
+            this.barManager1.Form = this;
+            this.barManager1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.barButtonItem_Edit,
+            this.barButtonItem_Delete});
+            this.barManager1.MaxItemId = 2;
+            // 
+            // barDockControlTop
+            // 
+            this.barDockControlTop.CausesValidation = false;
+            this.barDockControlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.barDockControlTop.Location = new System.Drawing.Point(0, 0);
+            this.barDockControlTop.Manager = this.barManager1;
+            this.barDockControlTop.Size = new System.Drawing.Size(1015, 0);
+            // 
+            // barDockControlBottom
+            // 
+            this.barDockControlBottom.CausesValidation = false;
+            this.barDockControlBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.barDockControlBottom.Location = new System.Drawing.Point(0, 542);
+            this.barDockControlBottom.Manager = this.barManager1;
+            this.barDockControlBottom.Size = new System.Drawing.Size(1015, 0);
+            // 
+            // barDockControlLeft
+            // 
+            this.barDockControlLeft.CausesValidation = false;
+            this.barDockControlLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.barDockControlLeft.Location = new System.Drawing.Point(0, 0);
+            this.barDockControlLeft.Manager = this.barManager1;
+            this.barDockControlLeft.Size = new System.Drawing.Size(0, 542);
+            // 
+            // barDockControlRight
+            // 
+            this.barDockControlRight.CausesValidation = false;
+            this.barDockControlRight.Dock = System.Windows.Forms.DockStyle.Right;
+            this.barDockControlRight.Location = new System.Drawing.Point(1015, 0);
+            this.barDockControlRight.Manager = this.barManager1;
+            this.barDockControlRight.Size = new System.Drawing.Size(0, 542);
             // 
             // FormGridMenu
             // 
-            this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
-            this.ClientSize = new System.Drawing.Size(575, 317);
-            this.Controls.Add(this.radioGroup1);
+            this.AutoScaleBaseSize = new System.Drawing.Size(10, 26);
+            this.ClientSize = new System.Drawing.Size(1015, 542);
             this.Controls.Add(this.gridControl1);
+            this.Controls.Add(this.barDockControlLeft);
+            this.Controls.Add(this.barDockControlRight);
+            this.Controls.Add(this.barDockControlBottom);
+            this.Controls.Add(this.barDockControlTop);
             this.Name = "FormGridMenu";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.radioGroup1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.popupMenu_Row)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barManager1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private DevExpress.XtraGrid.GridControl gridControl1;
         private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
-        internal System.Windows.Forms.ContextMenu ContextMenu1;
-        internal System.Windows.Forms.MenuItem MenuItem1;
-        internal System.Windows.Forms.MenuItem MenuItem2;
-        private DevExpress.XtraEditors.RadioGroup radioGroup1;
+        private DevExpress.XtraBars.PopupMenu popupMenu_Row;
+        private DevExpress.XtraBars.BarManager barManager1;
+        private DevExpress.XtraBars.BarDockControl barDockControlTop;
+        private DevExpress.XtraBars.BarDockControl barDockControlBottom;
+        private DevExpress.XtraBars.BarDockControl barDockControlLeft;
+        private DevExpress.XtraBars.BarDockControl barDockControlRight;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem_Edit;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem_Delete;
     }
 }
--- CS/FormGridMenu.cs
+++ CS/FormGridMenu.cs
@@ -1,57 +1,36 @@
 ﻿using DevExpress.Utils.Menu;
+using DevExpress.XtraBars;
 using DevExpress.XtraGrid.Views.Grid;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Windows.Forms;
+using DevExpress.XtraGrid.Views.Grid.ViewInfo;
 
-namespace RowsPopupMenu
-{
-    public partial class FormGridMenu : Form
-    {
-        public FormGridMenu()
-        {
+namespace RowsPopupMenu {
+    public partial class FormGridMenu : DevExpress.XtraEditors.XtraForm {
+        public FormGridMenu() {
             InitializeComponent();
-            InitializeMenuItems();
         }
-
-        DXMenuItem[] menuItems;
-        void InitializeMenuItems()
-        {
-            DXMenuItem itemEdit = new DXMenuItem("Edit", ItemEdit_Click);
-            DXMenuItem itemDelete = new DXMenuItem("Delete", ItemDelete_Click);
-            menuItems = new DXMenuItem[] { itemEdit, itemDelete };
-        }
-
-        private void gridView1_PopupMenuShowing(object sender, PopupMenuShowingEventArgs e)
-        {
-            if (e.HitInfo.InRow)
-            {
-                GridView view = sender as GridView;
-                view.FocusedRowHandle = e.HitInfo.RowHandle;
-
-                if (radioGroup1.EditValue.ToString() == "Standard Menu")
-                    ContextMenu1.Show(view.GridControl, e.Point);
-
-                if (radioGroup1.EditValue.ToString() == "DevExpress Menu") {
-                    foreach (DXMenuItem item in menuItems)
-                        e.Menu.Items.Add(item);
-                }
-            }
-        }
-        
-
-        private void Form1_Load(object sender, System.EventArgs e)
-        {
+        private void Form1_Load(object sender, System.EventArgs e) {
             new DevExpress.XtraGrid.Design.XViewsPrinting(gridControl1);
         }
-        private void ItemEdit_Click(object sender, System.EventArgs e)
-        {
-            gridView1.ShowEditor();
+
+        private void gridView1_PopupMenuShowing(object sender, PopupMenuShowingEventArgs e) {
+            if (e.MenuType == GridMenuType.Row) {
+                popupMenu_Row.Tag = e.HitInfo;
+                e.ShowCustomMenu(popupMenu_Row);
+            }
         }
-        private void ItemDelete_Click(object sender, System.EventArgs e)
-        {
-            gridView1.DeleteRow(gridView1.FocusedRowHandle);
+        GridHitInfo GetHitInfo(BarItemLink link) {
+            PopupMenu menu = link.LinkedObject as PopupMenu;
+            return menu.Tag as GridHitInfo;
+        }
+
+        private void barButtonItem_Edit_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e) {
+            GridHitInfo info = GetHitInfo(e.Link);
+            info.View.ShowEditor();
+        }
+
+        private void barButtonItem_Delete_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e) {
+            GridHitInfo info = GetHitInfo(e.Link);
+            info.View.DeleteRow(info.View.FocusedRowHandle);
         }
     }
 }
