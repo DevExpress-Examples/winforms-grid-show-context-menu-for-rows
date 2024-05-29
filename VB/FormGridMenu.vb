d76e70e8b6cf4ada3909cf0fae9d977854062911
SunkarGG <sunkar.galikhanov@devexpress.com>
2024-05-29 18:23:49 +0500

    Add files via upload

--- VB/FormGridMenu.vb
+++ VB/FormGridMenu.vb
@@ -1,49 +1,37 @@
-Imports DevExpress.Utils.Menu
+﻿Imports System
+Imports DevExpress.XtraBars
 Imports DevExpress.XtraGrid.Views.Grid
-Imports System
-Imports System.Windows.Forms
+Imports DevExpress.XtraGrid.Views.Grid.ViewInfo
 
 Namespace RowsPopupMenu
-
     Public Partial Class FormGridMenu
-        Inherits Form
-
+        Inherits DevExpress.XtraEditors.XtraForm
         Public Sub New()
             InitializeComponent()
-            InitializeMenuItems()
         End Sub
-
-        Private menuItems As DXMenuItem()
-
-        Private Sub InitializeMenuItems()
-            Dim itemEdit As DXMenuItem = New DXMenuItem("Edit", AddressOf ItemEdit_Click)
-            Dim itemDelete As DXMenuItem = New DXMenuItem("Delete", AddressOf ItemDelete_Click)
-            menuItems = New DXMenuItem() {itemEdit, itemDelete}
-        End Sub
-
-        Private Sub gridView1_PopupMenuShowing(ByVal sender As Object, ByVal e As PopupMenuShowingEventArgs)
-            If e.HitInfo.InRow Then
-                Dim view As GridView = TryCast(sender, GridView)
-                view.FocusedRowHandle = e.HitInfo.RowHandle
-                If Equals(radioGroup1.EditValue.ToString(), "Standard Menu") Then ContextMenu1.Show(view.GridControl, e.Point)
-                If Equals(radioGroup1.EditValue.ToString(), "DevExpress Menu") Then
-                    For Each item As DXMenuItem In menuItems
-                        e.Menu.Items.Add(item)
-                    Next
-                End If
-            End If
-        End Sub
-
-        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+        Private Sub Form1_Load(sender As Object, e As EventArgs)
             Dim tmp_XViewsPrinting = New DevExpress.XtraGrid.Design.XViewsPrinting(gridControl1)
         End Sub
 
-        Private Sub ItemEdit_Click(ByVal sender As Object, ByVal e As EventArgs)
-            gridView1.ShowEditor()
+        Private Sub gridView1_PopupMenuShowing(sender As Object, e As PopupMenuShowingEventArgs)
+            If e.MenuType = GridMenuType.Row Then
+                popupMenu_Row.Tag = e.HitInfo
+                e.ShowCustomMenu(popupMenu_Row)
+            End If
+        End Sub
+        Private Function GetHitInfo(link As BarItemLink) As GridHitInfo
+            Dim menu As PopupMenu = TryCast(link.LinkedObject, PopupMenu)
+            Return TryCast(menu.Tag, GridHitInfo)
+        End Function
+
+        Private Sub barButtonItem_Edit_ItemClick(sender As Object, e As ItemClickEventArgs)
+            Dim info = GetHitInfo(e.Link)
+            info?.View.ShowEditor()
         End Sub
 
-        Private Sub ItemDelete_Click(ByVal sender As Object, ByVal e As EventArgs)
-            gridView1.DeleteRow(gridView1.FocusedRowHandle)
+        Private Sub barButtonItem_Delete_ItemClick(sender As Object, e As ItemClickEventArgs)
+            Dim info = GetHitInfo(e.Link)
+            info.View.DeleteRow(info.View.FocusedRowHandle)
         End Sub
     End Class
 End Namespace
