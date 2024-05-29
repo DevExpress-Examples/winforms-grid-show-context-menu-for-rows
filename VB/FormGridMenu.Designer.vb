8444065d81745d6b136065962cc85516d42642b9
DevExpressExampleBot <fakeEmail>
2024-05-29 17:25:17 +0400

    Source auto update [skip ci]

--- VB/FormGridMenu.Designer.vb
+++ VB/FormGridMenu.Designer.vb
@@ -1,159 +1,108 @@
-﻿Imports System
-
 Namespace RowsPopupMenu
+
     Partial Class FormGridMenu
+
         ''' <summary>
         ''' Required designer variable.
         ''' </summary>
-        Private components As ComponentModel.IContainer = Nothing
+        Private components As System.ComponentModel.IContainer = Nothing
 
         ''' <summary>
         ''' Clean up any resources being used.
         ''' </summary>
-        ''' <paramname="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        Protected Overrides Sub Dispose(disposing As Boolean)
-            If disposing AndAlso components IsNot Nothing Then
-                components.Dispose()
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
             End If
+
             MyBase.Dispose(disposing)
         End Sub
 
 #Region "Windows Form Designer generated code"
-
         ''' <summary>
         ''' Required method for Designer support - do not modify
         ''' the contents of this method with the code editor.
         ''' </summary>
         Private Sub InitializeComponent()
-            components = New ComponentModel.Container()
-            Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(FormGridMenu))
-            gridControl1 = New DevExpress.XtraGrid.GridControl()
-            gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
-            popupMenu_Row = New DevExpress.XtraBars.PopupMenu(components)
-            barButtonItem_Edit = New DevExpress.XtraBars.BarButtonItem()
-            barButtonItem_Delete = New DevExpress.XtraBars.BarButtonItem()
-            barManager1 = New DevExpress.XtraBars.BarManager(components)
-            barDockControlTop = New DevExpress.XtraBars.BarDockControl()
-            barDockControlBottom = New DevExpress.XtraBars.BarDockControl()
-            barDockControlLeft = New DevExpress.XtraBars.BarDockControl()
-            barDockControlRight = New DevExpress.XtraBars.BarDockControl()
-            CType(gridControl1, ComponentModel.ISupportInitialize).BeginInit()
-            CType(gridView1, ComponentModel.ISupportInitialize).BeginInit()
-            CType(popupMenu_Row, ComponentModel.ISupportInitialize).BeginInit()
-            CType(barManager1, ComponentModel.ISupportInitialize).BeginInit()
-            SuspendLayout()
+            Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
+            Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
+            Me.ContextMenu1 = New System.Windows.Forms.ContextMenu()
+            Me.MenuItem1 = New System.Windows.Forms.MenuItem()
+            Me.MenuItem2 = New System.Windows.Forms.MenuItem()
+            Me.radioGroup1 = New DevExpress.XtraEditors.RadioGroup()
+            CType((Me.gridControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.gridView1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.radioGroup1.Properties), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
             ' 
             ' gridControl1
             ' 
-            gridControl1.Dock = Windows.Forms.DockStyle.Fill
-            gridControl1.Location = New Drawing.Point(0, 0)
-            gridControl1.MainView = gridView1
-            gridControl1.Name = "gridControl1"
-            gridControl1.Size = New Drawing.Size(1015, 542)
-            gridControl1.TabIndex = 0
-            gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {gridView1})
+            Me.gridControl1.Dock = System.Windows.Forms.DockStyle.Left
+            Me.gridControl1.Location = New System.Drawing.Point(0, 0)
+            Me.gridControl1.MainView = Me.gridView1
+            Me.gridControl1.Name = "gridControl1"
+            Me.gridControl1.Size = New System.Drawing.Size(448, 317)
+            Me.gridControl1.TabIndex = 0
+            Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.gridView1})
             ' 
             ' gridView1
             ' 
-            gridView1.GridControl = gridControl1
-            gridView1.Name = "gridView1"
-            AddHandler gridView1.PopupMenuShowing, New DevExpress.XtraGrid.Views.Grid.PopupMenuShowingEventHandler(AddressOf gridView1_PopupMenuShowing)
+            Me.gridView1.GridControl = Me.gridControl1
+            Me.gridView1.Name = "gridView1"
+            AddHandler Me.gridView1.PopupMenuShowing, New DevExpress.XtraGrid.Views.Grid.PopupMenuShowingEventHandler(AddressOf Me.gridView1_PopupMenuShowing)
             ' 
-            ' popupMenu_Row
+            ' ContextMenu1
             ' 
-            popupMenu_Row.LinksPersistInfo.AddRange(New DevExpress.XtraBars.LinkPersistInfo() {New DevExpress.XtraBars.LinkPersistInfo(barButtonItem_Edit), New DevExpress.XtraBars.LinkPersistInfo(barButtonItem_Delete)})
-            popupMenu_Row.Manager = barManager1
-            popupMenu_Row.Name = "popupMenu_Row"
+            Me.ContextMenu1.MenuItems.AddRange(New System.Windows.Forms.MenuItem() {Me.MenuItem1, Me.MenuItem2})
             ' 
-            ' barButtonItem_Edit
+            ' MenuItem1
             ' 
-            barButtonItem_Edit.Caption = "Edit"
-            barButtonItem_Edit.Id = 0
-            barButtonItem_Edit.ImageOptions.SvgImage = CType(resources.GetObject("barButtonItem_Edit.ImageOptions.SvgImage"), DevExpress.Utils.Svg.SvgImage)
-            barButtonItem_Edit.Name = "barButtonItem_Edit"
-            AddHandler barButtonItem_Edit.ItemClick, New DevExpress.XtraBars.ItemClickEventHandler(AddressOf barButtonItem_Edit_ItemClick)
+            Me.MenuItem1.Index = 0
+            Me.MenuItem1.Text = "Edit"
+            AddHandler Me.MenuItem1.Click, New System.EventHandler(AddressOf Me.ItemEdit_Click)
             ' 
-            ' barButtonItem_Delete
+            ' MenuItem2
             ' 
-            barButtonItem_Delete.Caption = "Delete"
-            barButtonItem_Delete.Id = 1
-            barButtonItem_Delete.ImageOptions.SvgImage = CType(resources.GetObject("barButtonItem_Delete.ImageOptions.SvgImage"), DevExpress.Utils.Svg.SvgImage)
-            barButtonItem_Delete.Name = "barButtonItem_Delete"
-            AddHandler barButtonItem_Delete.ItemClick, New DevExpress.XtraBars.ItemClickEventHandler(AddressOf barButtonItem_Delete_ItemClick)
+            Me.MenuItem2.Index = 1
+            Me.MenuItem2.Text = "Delete"
+            AddHandler Me.MenuItem2.Click, New System.EventHandler(AddressOf Me.ItemDelete_Click)
             ' 
-            ' barManager1
+            ' radioGroup1
             ' 
-            barManager1.DockControls.Add(barDockControlTop)
-            barManager1.DockControls.Add(barDockControlBottom)
-            barManager1.DockControls.Add(barDockControlLeft)
-            barManager1.DockControls.Add(barDockControlRight)
-            barManager1.Form = Me
-            barManager1.Items.AddRange(New DevExpress.XtraBars.BarItem() {barButtonItem_Edit, barButtonItem_Delete})
-            barManager1.MaxItemId = 2
-            ' 
-            ' barDockControlTop
-            ' 
-            barDockControlTop.CausesValidation = False
-            barDockControlTop.Dock = Windows.Forms.DockStyle.Top
-            barDockControlTop.Location = New Drawing.Point(0, 0)
-            barDockControlTop.Manager = barManager1
-            barDockControlTop.Size = New Drawing.Size(1015, 0)
-            ' 
-            ' barDockControlBottom
-            ' 
-            barDockControlBottom.CausesValidation = False
-            barDockControlBottom.Dock = Windows.Forms.DockStyle.Bottom
-            barDockControlBottom.Location = New Drawing.Point(0, 542)
-            barDockControlBottom.Manager = barManager1
-            barDockControlBottom.Size = New Drawing.Size(1015, 0)
-            ' 
-            ' barDockControlLeft
-            ' 
-            barDockControlLeft.CausesValidation = False
-            barDockControlLeft.Dock = Windows.Forms.DockStyle.Left
-            barDockControlLeft.Location = New Drawing.Point(0, 0)
-            barDockControlLeft.Manager = barManager1
-            barDockControlLeft.Size = New Drawing.Size(0, 542)
-            ' 
-            ' barDockControlRight
-            ' 
-            barDockControlRight.CausesValidation = False
-            barDockControlRight.Dock = Windows.Forms.DockStyle.Right
-            barDockControlRight.Location = New Drawing.Point(1015, 0)
-            barDockControlRight.Manager = barManager1
-            barDockControlRight.Size = New Drawing.Size(0, 542)
+            Me.radioGroup1.EditValue = "DevExpress Menu"
+            Me.radioGroup1.Location = New System.Drawing.Point(454, 12)
+            Me.radioGroup1.Name = "radioGroup1"
+            Me.radioGroup1.Properties.Items.AddRange(New DevExpress.XtraEditors.Controls.RadioGroupItem() {New DevExpress.XtraEditors.Controls.RadioGroupItem("Standard Menu", "Standard Menu"), New DevExpress.XtraEditors.Controls.RadioGroupItem("DevExpress Menu", "DevExpress Menu")})
+            Me.radioGroup1.Size = New System.Drawing.Size(116, 80)
+            Me.radioGroup1.TabIndex = 1
             ' 
             ' FormGridMenu
             ' 
-            AutoScaleBaseSize = New Drawing.Size(10, 26)
-            ClientSize = New Drawing.Size(1015, 542)
-            Controls.Add(gridControl1)
-            Controls.Add(barDockControlLeft)
-            Controls.Add(barDockControlRight)
-            Controls.Add(barDockControlBottom)
-            Controls.Add(barDockControlTop)
-            Name = "FormGridMenu"
-            Text = "Form1"
-            AddHandler Load, New EventHandler(AddressOf Form1_Load)
-            CType(gridControl1, ComponentModel.ISupportInitialize).EndInit()
-            CType(gridView1, ComponentModel.ISupportInitialize).EndInit()
-            CType(popupMenu_Row, ComponentModel.ISupportInitialize).EndInit()
-            CType(barManager1, ComponentModel.ISupportInitialize).EndInit()
-            ResumeLayout(False)
-            PerformLayout()
-
+            Me.AutoScaleBaseSize = New System.Drawing.Size(5, 13)
+            Me.ClientSize = New System.Drawing.Size(575, 317)
+            Me.Controls.Add(Me.radioGroup1)
+            Me.Controls.Add(Me.gridControl1)
+            Me.Name = "FormGridMenu"
+            Me.Text = "Form1"
+            AddHandler Me.Load, New System.EventHandler(AddressOf Me.Form1_Load)
+            CType((Me.gridControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.gridView1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.radioGroup1.Properties), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
         End Sub
 
 #End Region
         Private gridControl1 As DevExpress.XtraGrid.GridControl
+
         Private gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
-        Private popupMenu_Row As DevExpress.XtraBars.PopupMenu
-        Private barManager1 As DevExpress.XtraBars.BarManager
-        Private barDockControlTop As DevExpress.XtraBars.BarDockControl
-        Private barDockControlBottom As DevExpress.XtraBars.BarDockControl
-        Private barDockControlLeft As DevExpress.XtraBars.BarDockControl
-        Private barDockControlRight As DevExpress.XtraBars.BarDockControl
-        Private barButtonItem_Edit As DevExpress.XtraBars.BarButtonItem
-        Private barButtonItem_Delete As DevExpress.XtraBars.BarButtonItem
+
+        Friend ContextMenu1 As System.Windows.Forms.ContextMenu
+
+        Friend MenuItem1 As System.Windows.Forms.MenuItem
+
+        Friend MenuItem2 As System.Windows.Forms.MenuItem
+
+        Private radioGroup1 As DevExpress.XtraEditors.RadioGroup
     End Class
 End Namespace
--- VB/FormGridMenu.vb
+++ VB/FormGridMenu.vb
@@ -1,37 +1,49 @@
-﻿Imports System
-Imports DevExpress.XtraBars
+Imports DevExpress.Utils.Menu
 Imports DevExpress.XtraGrid.Views.Grid
-Imports DevExpress.XtraGrid.Views.Grid.ViewInfo
+Imports System
+Imports System.Windows.Forms
 
 Namespace RowsPopupMenu
+
     Public Partial Class FormGridMenu
-        Inherits DevExpress.XtraEditors.XtraForm
+        Inherits Form
+
         Public Sub New()
             InitializeComponent()
+            InitializeMenuItems()
         End Sub
-        Private Sub Form1_Load(sender As Object, e As EventArgs)
+
+        Private menuItems As DXMenuItem()
+
+        Private Sub InitializeMenuItems()
+            Dim itemEdit As DXMenuItem = New DXMenuItem("Edit", AddressOf ItemEdit_Click)
+            Dim itemDelete As DXMenuItem = New DXMenuItem("Delete", AddressOf ItemDelete_Click)
+            menuItems = New DXMenuItem() {itemEdit, itemDelete}
+        End Sub
+
+        Private Sub gridView1_PopupMenuShowing(ByVal sender As Object, ByVal e As PopupMenuShowingEventArgs)
+            If e.HitInfo.InRow Then
+                Dim view As GridView = TryCast(sender, GridView)
+                view.FocusedRowHandle = e.HitInfo.RowHandle
+                If Equals(radioGroup1.EditValue.ToString(), "Standard Menu") Then ContextMenu1.Show(view.GridControl, e.Point)
+                If Equals(radioGroup1.EditValue.ToString(), "DevExpress Menu") Then
+                    For Each item As DXMenuItem In menuItems
+                        e.Menu.Items.Add(item)
+                    Next
+                End If
+            End If
+        End Sub
+
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
             Dim tmp_XViewsPrinting = New DevExpress.XtraGrid.Design.XViewsPrinting(gridControl1)
         End Sub
 
-        Private Sub gridView1_PopupMenuShowing(sender As Object, e As PopupMenuShowingEventArgs)
-            If e.MenuType = GridMenuType.Row Then
-                popupMenu_Row.Tag = e.HitInfo
-                e.ShowCustomMenu(popupMenu_Row)
-            End If
-        End Sub
-        Private Function GetHitInfo(link As BarItemLink) As GridHitInfo
-            Dim menu As PopupMenu = TryCast(link.LinkedObject, PopupMenu)
-            Return TryCast(menu.Tag, GridHitInfo)
-        End Function
-
-        Private Sub barButtonItem_Edit_ItemClick(sender As Object, e As ItemClickEventArgs)
-            Dim info = GetHitInfo(e.Link)
-            info?.View.ShowEditor()
+        Private Sub ItemEdit_Click(ByVal sender As Object, ByVal e As EventArgs)
+            gridView1.ShowEditor()
         End Sub
 
-        Private Sub barButtonItem_Delete_ItemClick(sender As Object, e As ItemClickEventArgs)
-            Dim info = GetHitInfo(e.Link)
-            info.View.DeleteRow(info.View.FocusedRowHandle)
+        Private Sub ItemDelete_Click(ByVal sender As Object, ByVal e As EventArgs)
+            gridView1.DeleteRow(gridView1.FocusedRowHandle)
         End Sub
     End Class
 End Namespace
